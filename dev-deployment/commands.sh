2b60d7bc4dd3551e04f0ee9dcde36167a6f1826a
tmax22 <el96m8ro@p12test.onmicrosoft.com>
2023-12-14 16:48:41 +0200

    local docker dev

--- dev-deployment/commands.sh
+++ dev-deployment/commands.sh
@@ -1,4 +1,4 @@
-docker compose -p reportportal-dev up -d
+docker compose -p reportportal-dev up -d --build --no-deps --force-recreate
 
 # run only ui service
 docker compose -p reportportal-dev up ui --build --no-deps --force-recreate
